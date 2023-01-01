22dd5f04e87f6c862ffa4893a9a4b9e8860a3ce3
Mateus Pragana <mateuspragana@gmail.com>
2023-01-01 20:53:51 -0300

    Finalizando aula: Estrutura da Home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
 import logoImg from '../assets/logo.svg'
 import { globalStyles } from '../styles/global'
 import { Container, Header } from '../styles/pages/app'
-
-import Image from 'next/image'
 
 globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
