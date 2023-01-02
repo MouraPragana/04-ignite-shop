f23db87a751467ed569e6ee68c5f23733f16b3fc
Mateus Pragana <mateuspragana@gmail.com>
2023-01-01 21:55:28 -0300

    Finalizando aula: Criando carrossel + alterações próprias

--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -3,14 +3,15 @@ import { styled } from '..'
 export const Container = styled('div', {
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'flex-start',
-  justifyContent: 'center',
   minHeight: '100vh',
 })
 
 export const Header = styled('header', {
+  display: 'flex',
   padding: '2rem 0',
   width: '100%',
+  height: '120px',
   maxWidth: 1180,
   margin: '0 auto',
+  alignItems: 'center',
 })
