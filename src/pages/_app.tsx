ac2807711ecf62ffd7c3065bc3ad29aa5de43f34
Mateus Moura <mateuspragana@gmail.com>
2023-07-28 09:08:03 -0300

    feat: implementando o use-shopping-cart

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { NextPage } from 'next'
 import type { AppProps } from 'next/app'
 import { ReactElement, ReactNode } from 'react'
 import { globalStyles } from '../styles/global'
+import { CartProvider } from 'use-shopping-cart'
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
@@ -14,5 +15,15 @@ type AppPropsWithLayout = AppProps & {
 globalStyles()
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page)
-  return getLayout(<Component {...pageProps} />)
+  return (
+    <CartProvider
+      cartMode="checkout-session"
+      stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY}
+      currency="BRL"
+      persistKey="@Stripe"
+      shouldPersist={true}
+    >
+      {getLayout(<Component {...pageProps} />)}
+    </CartProvider>
+  )
 }
